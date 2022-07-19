9abf61ada15173839a00047bef668a2f171e12e0
Francisco Gonzalez <frangc0206@gmail.com>
2022-07-19 20:02:17 -0300

    Created individual + all drivers display

--- src/pages/drivers/[id].tsx
+++ src/pages/drivers/[id].tsx
@@ -3,13 +3,22 @@ import React from "react";
 import Containter from "../../utils/Containter";
 import { PrismaClient } from "@prisma/client";
 import { Pilot } from "../../types/driver";
+import FullpageDriver from "../../Components/Drivers/FullpageDriver";
+import Head from "next/head";
 
 const prisma = new PrismaClient();
 
-const Details: NextPage = ({ driver }: any) => {
-  return <Containter>
-    {driver.name}
-  </Containter>;
+const FullDriver: NextPage = ({ driver, team }: any) => {
+  return (
+    <>
+      <Head>
+        <title>{`${driver.name} ${driver.surname} | ${team.full_name}`}</title>
+      </Head>
+      <Containter>
+        <FullpageDriver driver={driver} team={team} />
+      </Containter>
+    </>
+  );
 };
 
 export const getStaticPaths = async () => {
@@ -23,20 +32,25 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 };
 
 export const getStaticProps = async (context: any) => {
-    const id = context.params.id
-    const driver = await prisma.pilot.findUnique({
-        where: {
-            id,
-        }
-    })
-    return {
-        props: { driver }
-    }
-}
+  const id = context.params.id;
+  const driver = await prisma.pilot.findUnique({
+    where: {
+      id,
+    },
+  });
+  const team = await prisma.team.findUnique({
+    where: {
+      id: driver?.teamId,
+    },
+  });
+  return {
+    props: { driver, team },
+  };
+};
 
-export default Details;
+export default FullDriver;
---
+++ src/pages/drivers/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Containter from "../../utils/Containter";
+import { PrismaClient } from "@prisma/client";
+import { Pilot, Team } from "../../types/driver";
+import DriverCard from "../../Components/utils/DriverCard";
+import Head from "next/head";
+
+const prisma = new PrismaClient();
+
+const AllDrivers = (props: any) => {
+  const drivers: Pilot[] = props.initialDrivers;
+  const teams: Team[] = props.initialTeams;
+
+  return (
+    <>
+    <Head>
+        <title>All Drivers</title>
+    </Head>
+    <Containter>
+      <div className="grid gap-4 grid-cols-5 grid-rows-4">
+        {drivers.map((driver, index) => {
+            const team = teams.find((team) => team.id === driver.teamId)!
+            return <DriverCard key={index} driver={driver} team={team} />;
+        })}
+      </div>
+    </Containter>
+        </>
+  );
+};
+
+export async function getStaticProps() {
+  const drivers = await prisma.pilot.findMany();
+  const teams = await prisma.team.findMany();
+  return {
+    props: {
+      initialDrivers: drivers,
+      initialTeams: teams,
+    },
+  };
+}
+
+export default AllDrivers;
