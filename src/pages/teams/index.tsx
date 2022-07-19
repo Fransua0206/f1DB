3cf6dfb4dc951fac64db3296833bb3fc75c2c0bc
Francisco Gonzalez <frangc0206@gmail.com>
2022-07-19 20:05:50 -0300

    Updated title

--- src/pages/teams/index.tsx
+++ src/pages/teams/index.tsx
@@ -3,18 +3,24 @@ import Containter from "../../utils/Containter";
 import { PrismaClient } from "@prisma/client";
 import { Team } from "../../types/driver";
 import TeamCard from "../../Components/utils/TeamCard";
+import Head from "next/head";
 const prisma = new PrismaClient();
 
 const AllTeams = (props: any) => {
   const teams: Team[] = props.initialTeams;
   return (
-    <Containter>
-      <div className="grid grid-cols-5 grid-rows-2">
-        {teams.map((team, index) => {
-          return <TeamCard team={team} key={index} />;
-        })}
-      </div>
-    </Containter>
+    <>
+      <Head>
+        <title>All Teams</title>
+      </Head>
+      <Containter>
+        <div className="grid grid-cols-5 grid-rows-2">
+          {teams.map((team, index) => {
+            return <TeamCard team={team} key={index} />;
+          })}
+        </div>
+      </Containter>
+    </>
   );
 };
 
